dbeaef4141cd89f25d4d0e17383f064a0a7b202d
Berkant Gul <berkantgul25@gmail.com>
2022-12-23 15:47:54 +0300

    preparing the first partialviews

--- blog.webui/Startup.cs
+++ blog.webui/Startup.cs
@@ -54,7 +54,7 @@ namespace blog.webui
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
         }
     }
