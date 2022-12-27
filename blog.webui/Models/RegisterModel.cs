82ea0b61da6e863a8f0095d60ac10641bc64f7c4
Berkant Gul <berkantgul25@gmail.com>
2022-12-27 22:57:24 +0300

    CategoryList ViewComponent done

--- blog.webui/Models/RegisterModel.cs
+++ blog.webui/Models/RegisterModel.cs
@@ -10,13 +10,14 @@ namespace blog.webui.Models
     {
         [Required(ErrorMessage = "Name alanı boş geçilemez!")]
         [StringLength(maximumLength: 20, MinimumLength = 1, ErrorMessage = "Name alanı 1-20 karakter arası olmalıdır!")]
+        [Display(Name = "Author Name")]
         public string AutorName { get; set; }
         [Required(ErrorMessage = "Name alanı boş geçilemez!")]
         [StringLength(maximumLength: 30, MinimumLength = 7, ErrorMessage = "Name alanı 7-30 karakter arası olmalıdır!")]
         [DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
         [Required(ErrorMessage = "Şifre alanı boş geçilemez!")]
-        // [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*+\\/|!\"£$%^&*()#[@~'?><,.=_-]).{6,}$", ErrorMessage = "Şifre alanı alphanumeric değerler içermelidir!")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$", ErrorMessage = "Şifre alanı alphanumeric değerler içermelidir!")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
